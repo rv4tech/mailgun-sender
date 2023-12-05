1992e7d49c412b3d13b9a851fa53e4c7f25888c8
Andrew Taker <andrewtaker@Andrews-MacBook-Pro.local>
2023-12-05 21:46:03 +0300

    new branch

---
+++ main.go
@@ -0,0 +1,98 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	cmd "rv4-request/cmd_arguments"
+	"rv4-request/database"
+	"rv4-request/io"
+	"rv4-request/sender"
+
+	"github.com/joho/godotenv"
+	"gorm.io/gorm"
+)
+
+func init() {
+	// Get dotenv variables from .env file.
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+func main() {
+	var db *gorm.DB = database.InitDataBase("test.db")
+	var domain string = os.Getenv("DOMAIN")
+	var apiKey string = os.Getenv("APIKEY")
+	// Get cmd arguments from -ml and -camp commands.
+	var fileName, campaignName string = cmd.ParseArguments()
+	// Get initial database data based on passed cmd arguments.
+	var campaign database.Campaign
+	if err := db.Where("name = ?", campaignName).First(&campaign).Error; err != nil {
+		log.Fatalln("Error querying for campaign", err)
+	}
+
+	// Structured slice of clients read from csv file.
+	clients := CreateClientsSlice(io.ReadCsvFile(fileName))
+
+	for _, client := range clients {
+		// Payload for MailGun message sending function.
+		var payload sender.MailGunPayload
+		// Struct to enscapsulate data for 'send_stats' table.
+		var stats database.SendStat
+
+		// Check if row with requested 'camp_id' and 'lang' exists.
+		// If not - we use campaign's default language.
+		translation, translationDoesNotExist := database.TranslationDoesNotExist(db, campaign.ID, client.Language)
+		if translationDoesNotExist {
+			// Dereferrence new db entity for 'translation' struct.
+			db.Where("camp_id = ? AND lang = ?", campaign.ID, campaign.DefaultLanguage).First(&translation)
+		}
+
+		// Create payload to fill MailGun send message function.
+		payload.From = translation.From
+		payload.Subject = translation.Subject
+		payload.Text = fmt.Sprintf("This message is supposed to be in <%s> language", translation.Language)
+		payload.To = client.Email
+		payload.Tags = append(payload.Tags, translation.Language, campaign.MailgunTemplate)
+		payload.TemplateVersion = translation.Language
+		payload.TemplateName = campaign.MailgunTemplate
+
+		// Create some of statistics variables.
+		// We do not initialize 'ErrorMessage' and 'Success' until after we send message.
+		stats.CampaignID = campaign.ID
+		stats.Email = client.Email
+		stats.ExternalID = client.ExternalID
+		stats.Language = translation.Language
+
+		// Check if message was sent already by three parameters.
+		// First is 'camp_id'.
+		// Second is 'email'.
+		// Third is 'success'. Must be true (1).
+		// Even if 'camp_id' and 'email' already exists, but success was false (0), we send the message.
+		statExists, statID := database.StatExists(db, &stats)
+		if statExists {
+			WriteLog("ERROR", fmt.Sprintf("Message for <%s> was not sent. Entry with this statistic already exists with id %v", client.Email, statID))
+			stats.ErrorMessage = fmt.Sprintf("already sent <%v>", statID)
+			stats.Success = false
+			db.Create(&stats)
+			continue
+		}
+
+		// MailGun response enteties.
+		// responseMessage contains basic info about request. If message was sent successfully, it return "Queued. Thank you."
+		// responseID contains MailGun message ID.
+		// responseError contains error message if any occurs.
+		responseMessage, responseID, responseError := sender.SendMailGunMessageV4(domain, apiKey, &payload)
+		if responseError != nil {
+			WriteLog("ERROR", fmt.Sprintf("Message for <%s> was not sent. MailGun responded with error: %s", client.Email, responseError))
+			stats.ErrorMessage = fmt.Sprint(responseError)
+			stats.Success = false
+		} else {
+			WriteLog("INFO", fmt.Sprintf("Message for <%s>:%s was successfully sent with %s lang. MG message: %s MG id: %s", client.Email, client.Language, translation.Language, responseMessage, responseID))
+			stats.ErrorMessage = ""
+			stats.Success = true
+		}
+		db.Create(&stats)
+	}
+}
---
+++ sender/sender.go
@@ -0,0 +1,46 @@
+package sender
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
+
+	"github.com/mailgun/mailgun-go/v4"
+)
+
+type MailGunPayload struct {
+	From            string
+	Subject         string
+	Text            string
+	TemplateVersion string // t:version
+	TemplateName    string
+	To              string
+	Tags            []string // o:tag
+}
+
+// Sends message using Mailgun API to a SINGLE recipient.
+func SendMailGunMessageV4(domain, apiKey string, payload *MailGunPayload) (string, string, error) {
+	mailgun := mailgun.NewMailgun(domain, apiKey)
+	// Empty messages return "message not valid" from MailGun.
+	if payload.Text == "" {
+		payload.Text = " "
+	}
+	message := mailgun.NewMessage(payload.From, payload.Subject, payload.Text, payload.To)
+	// Set correct template version (language).
+	message.SetTemplate(payload.TemplateName)
+	message.SetTemplateVersion(payload.TemplateVersion)
+	// Add tags.
+	for _, tag := range payload.Tags {
+		err := message.AddTag(tag)
+		if err != nil {
+			fmt.Println(err)
+			log.Fatalf("Could not add a tag <%s>. Passed payload: %+v", tag, payload)
+		}
+	}
+	// Send message with timeout to ensure resources are realeased after message was sent.
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*30)
+	defer cancel()
+	responseMessage, id, err := mailgun.Send(ctx, message)
+	return responseMessage, id, err
+}
